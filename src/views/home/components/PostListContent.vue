084c909b438cffe39625a3c8acea2bcb97cd15e5
Ralph Maron Eda <edaralphmaron@gmail.com>
2025-03-21 11:39:23 +0800

    added more images and fixed postlistcontent not showing images

--- src/views/home/components/PostListContent.vue
+++ src/views/home/components/PostListContent.vue
@@ -1,15 +1,40 @@
 <script setup lang="ts">
-
+const post = [
+	{
+		id: 0,
+		title: 'Mountain',
+		date: 'March 13, 2025',
+		description: 'Mountain image.',
+		image: '/glimpse/img_20220913_1.jpg',
+		location: 'Earth'
+	},
+	{
+		id: 1,
+		title: 'Mountain',
+		date: 'March 13, 2025',
+		description: 'Mountain image.',
+		image: '/glimpse/img_20220919_1.jpg',
+		location: 'Earth'
+	},
+	{
+		id: 2,
+		title: 'Mountain',
+		date: 'March 13, 2025',
+		description: 'Mountain image.',
+		image: '/glimpse/img_20220920_1.jpg',
+		location: 'Earth'
+	},
+]
 </script>
 
 <template>
 	<div class="mt-1 col-lg-6 col-md-12 bg-light scrollable-column p-2">
-		<div class="mb-3" v-for="_ in 50">
+		<div class="mb-3" v-for="item in post" :key="item.id">
 			<div class="card px-3 pt-3">
-				<img src="/mountain.png" alt="Mountain" class="post-image">
-				<h5 class="text-secondary mt-3">Earth</h5>
-				<p class="text-secondary" style="font-size: smaller;">March 13, 2025</p>
-				<p class="text-secondary">Mountain image.</p>
+				<img :src="item.image" :alt="item.description" class="post-image">
+				<h5 class="text-secondary mt-3">{{ item.location }}</h5>
+				<p class="text-secondary" style="font-size: smaller;">{{ item.date }}</p>
+				<p class="text-secondary">{{ item.description }}</p>
 			</div>
 		</div>
 	</div>
@@ -21,5 +46,6 @@
 	height: auto;
 	object-fit: cover;
 	border-radius: 4px;
+	max-height: 320px;
 }
 </style>
